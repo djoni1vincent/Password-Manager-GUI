ae53adc2e8454888f06aecb5d29b4201446409a7
djoni1vincent <djoni8vincent@gmail.com>
2026-02-13 09:52:55 +0100

    Fead: add search existed credentials by typing website name

--- main.py
+++ main.py
@@ -44,9 +44,6 @@ def save_pass():
     if len(website_out) < 1 or len(password_out) < 4 or len(email_out) < 6:
         messagebox.showerror(message="You must fill in all the fields")
 
-    #    popup = messagebox.askokcancel(title="Information", message=f"These are the details: \nwebsite: {website_out}\nemail: {email_out} "
-    #                                        f"\npassword: {password_out}, \nIs it okay to save?")
-    #    if popup:
     else:
         try:
             with open("data.json", "r") as data_file:
@@ -61,6 +58,17 @@ def save_pass():
 
             website_entry.delete(0, "end")
             password_entry.delete(0, "end")
+
+def find_password():
+    with open("data.json", "r") as data_file:
+        data = json.load(data_file)
+        web = website_entry.get().title()
+        print(data[web]["email"])
+
+        if web in data:
+            pyperclip.copy(data[web]["password"])
+            popup = messagebox.showinfo(title="Information", message=f"These are the details: \nemail: {data[web]["email"]} "
+                                  f"\npassword: {data[web]["password"]}")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -79,15 +87,18 @@ canvas.grid(row=0, column=1, columnspan=2)
 #labels
 website_label = tk.Label(text="Website: ")
 website_label.grid(row=1, column=0)
-website_entry = tk.Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = tk.Entry(width=20)
+website_entry.grid(row=1, column=1, columnspan=1)
 website_entry.focus()
+
+search_button = tk.Button(text="Search", width=24, command=find_password)
+search_button.grid(row=1, column=2)
 
 email = tk.Label(text="Email/Username: ")
 email.grid(row=2, column=0)
 email_entry = tk.Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "djoni7vincent@gmail.com")
+email_entry.insert(0, "example@gmail.com")
 
 password = tk.Label(text="Password: ")
 password.grid(row=3, column=0)
@@ -100,28 +111,6 @@ button_generate.grid(row=3, column=2)
 
 button_add = tk.Button(text="Add", width=36, command=save_pass)
 button_add.grid(row=4, column=1, columnspan=2)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 window.mainloop()
